65855bb9d42357b9665e1d1a02721eb842eb0866
Deepali Verma <deepaliverma064@gmail.com>
2024-12-18 15:30:15 +0530

    adding payment route

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 import express from "express";
+import PaymentRoute from "./routes/payment.js";
 
 import { config } from "dotenv";
 
 config({ path: "./config/config.env" });
 
 export const app = express();
+
+app.use("/api", PaymentRoute)
